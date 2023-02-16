02040cf93c1bd3ce1f7c715a6f960e7e01736208
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-16 17:55:58 +0530

    Initial Redux Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import Counter from "./components/Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <Fragment>
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
